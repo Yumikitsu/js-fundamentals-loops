97040d3d4cfc5cc1bc689ecd849e7504a1ce253f
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-09-20 15:03:27 +0200

    Core, Extensions, and Advanced Completed!

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -6,20 +6,80 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 let hasTen = false
 let indexOfTen = -1
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 10) {
+    hasTen = true
+    indexOfTen = i
+    break
+  }
+}
+
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 3 === 0) {
+    divisibleByThreeCount++
+  }
+}
 
 // 3. use a for loop to find the average of the numbers in the array
 let average = 0
 
+for (let i = 0; i < nums.length; i++) {
+  average += nums[i] / nums.length
+}
+
 // 4. use a for loop to find the largest number in the array
 let largest = 0
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] > largest) {
+    largest = nums[i]
+  }
+}
 
 // 5. use a for loop to find the smallest number in the array
 let smallest = 100000
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] < smallest) {
+    smallest = nums[i]
+  }
+}
+
 // 6. find the median of the numbers in the array
 let median = 0
+
+const sortedNumsIndex = []
+
+// Create a sortedNumsIndex array which sorts the indicies of the nums array what elements needs to be where in an ascending order of the nums array
+for (let i = 0; i < nums.length; i++) {
+  let lowest = 100000
+  let indexOfLowest = -1
+  if (sortedNumsIndex.length > 0) {
+    lowest = nums[sortedNumsIndex.length - 1]
+  }
+  for (let j = 0; j < nums.length; j++) {
+    if (lowest >= nums[j] && !sortedNumsIndex.includes(j)) {
+      lowest = nums[j]
+      indexOfLowest = j
+    }
+  }
+  sortedNumsIndex.push(indexOfLowest)
+}
+
+// Calculate the number in the middle of the array
+const middle = Math.floor(nums.length / 2)
+
+// Get the median of the nums array
+if (nums.length % 2 === 0) {
+  median = Math.floor(
+    (nums[sortedNumsIndex[middle - 1]] + nums[sortedNumsIndex[middle]]) / 2
+  )
+} else {
+  median = nums[sortedNumsIndex[middle]]
+}
 
 module.exports = {
   hasTen,
--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -17,20 +17,81 @@ const deepThree = []
 // Your code here
 // }
 
+for (let i = 1; i <= 10; i++) {
+  simpleOne.push(i)
+}
+
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
+
+for (let i = 1; i <= 10; i++) {
+  const array = []
+  for (let j = 1; j <= i; j++) {
+    array.push(i)
+  }
+  nestedOne.push(array)
+}
 
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
 
+for (let i = 1; i <= 10; i++) {
+  const array = []
+  for (let j = i; j >= 1; j--) {
+    array.push(j)
+  }
+  nestedTwo.push(array)
+}
+
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+
+for (let i = 1; i <= 10; i++) {
+  const arrayTwo = []
+  for (let j = 1; j <= i; j++) {
+    const array = []
+    for (let k = 1; k <= j; k++) {
+      array.push(i)
+    }
+    arrayTwo.push(array)
+  }
+  deepOne.push(arrayTwo)
+}
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
 
+for (let i = 1; i <= 10; i++) {
+  const arrayTwo = []
+  for (let j = 1; j <= i; j++) {
+    const array = []
+    for (let k = 1; k <= j; k++) {
+      array.push(k)
+    }
+    arrayTwo.push(array)
+  }
+  deepTwo.push(arrayTwo)
+}
+
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+
+for (let i = 1; i <= 10; i++) {
+  const arrayTwo = []
+  for (let j = 1; j <= i; j++) {
+    const array = []
+    for (let k = 1; k <= j; k++) {
+      array.push(k)
+    }
+    let sumOfSquares = 0
+    for (let k = 0; k < array.length; k++) {
+      sumOfSquares += Math.pow(array[k], 2)
+    }
+    const average = sumOfSquares / array.length
+    arrayTwo.push([average])
+  }
+  deepThree.push(arrayTwo)
+}
 
 module.exports = {
   START,
